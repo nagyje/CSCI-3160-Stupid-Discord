2d78f4a961118d331e6edbe0c2165eca9e7f71e0
Joseph Nagy <102108245+nagyje@users.noreply.github.com>
2023-12-04 13:03:08 -0500

    Update server.c
    Added sleep(.1) to reduce resource load

--- server.c
+++ server.c
@@ -297,6 +297,9 @@ int main(int argc, char **argv){
 		// Add client to client queue and thread process
 		enqueue_client(client);
 		pthread_create(&pid, NULL, &manage_client, (void*)client);
+		
+		// Resource load reduction
+		sleep(0.1);
 	}
 
 	return EXIT_SUCCESS;
