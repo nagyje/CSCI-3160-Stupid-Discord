34a6164d7f827fe6372b7cf437256455d3dbbee9
Joseph Nagy <102108245+nagyje@users.noreply.github.com>
2023-12-05 14:06:49 -0500

    Update LogIn.py
    GUI Updates
    Added functionality for custom port
    Added timestamps on messages sent by user

--- LogIn.py
+++ LogIn.py
@@ -1,56 +1,44 @@
-"""
-Program Name: LogIn.py
-Author: Sophia Herrell
-Contributors: Joe Nagy 
-Last Edited: 12/5/23
-Description: Displays log in screen for stupid discord. 
-             Accepts name, port, and IP. 
-
-Additional Information:
-- This program is designed to be run from a WSL instance 
-  with python extensions installed. If you set up WSL in 
-  VS code, all you have to do is download the suggested
-  extensions. 
-- Launches Chat.py
-
-Usage:
-- You can run this program on its own using >$ python3 Chat.py, 
-  but it is designed to be run from LogIn.py. The main difference 
-  is that LogIn.py allows you to input the username <and port number>.  
-    - Otherwise, default username/port is Sophia/9001
-- You must have a server instance running on the same port for the 
-  client to connect to 
-"""
-
 #################### Import statements and constants ####################
 import tkinter as tk
 import subprocess
+import re
 
 MAX_USERNAME_LENGTH = 20
 DEFAULT_PORT = 9001
-DEFAULT_IP = "127.0.0.1"
 
 #################### Functions ####################
 
 def join_chat():
-    username = name_entry.get()
+    username = name_entry.get()  
     port = port_entry.get()
-    ip = ip_entry.get()
     
+    if not port:
+        port = DEFAULT_PORT
+        
+    # validate username and port
     if username:
         if len(username) <= MAX_USERNAME_LENGTH:
-            welcome_label.config(text=f"Welcome, {username}!")
-            launch_chat(username, port, ip)
+            if port:
+                if len(port) == 4:
+                    if re.match(r'^\d+$', port):
+                        welcome_label.config(text=f"Welcome, {username}!")
+                        launch_chat(username, port)
+                    else:
+                        welcome_label.config(text="Port must be characters 0-9")
+                else:
+                    welcome_label.config(text="Port must be 4 digits long.")
+            else:
+                welcome_label.config(text="Please enter a 4 digit port.")
         else:
             welcome_label.config(text="Names cannot be longer than 20 characters.")
     else:
         welcome_label.config(text="Please enter your name.")
         
-def launch_chat(username, port, ip):
+def launch_chat(username, port):
     # Close the Tkinter window
     logIn.destroy()
     # Launch the chat.py script with the provided username, port, and ip
-    subprocess.run(['python3', 'Chat.py', username, port, ip])
+    subprocess.run(['python3', 'Chat.py', username, port])
 
 #################### Main ####################
 logIn = tk.Tk()
@@ -96,22 +84,6 @@ def on_entry_click(event):
 
 # Bind the click event to the entry widget
 port_entry.bind('<FocusIn>', on_entry_click)
-
-# IP entry ----------------------
-# Create an entry widget for entering the IP with default text
-ip_default_text = "127.0.0.1"
-ip_entry = tk.Entry(input_frame, width=30, bg="#36393F", fg="white", insertbackground="white")
-ip_entry.insert(0, ip_default_text)  # Set default text
-ip_entry.pack(side=tk.TOP, padx=5)
-
-# Remove default text when the entry is clicked
-def on_entry_click(event):
-    if ip_entry.get() == ip_default_text:
-        ip_entry.delete(0, tk.END)
-        ip_entry.config(fg='white')  # Change text color to white
-
-# Bind the click event to the entry widget
-ip_entry.bind('<FocusIn>', on_entry_click)
 
 # Create a button to join
 join_button = tk.Button(input_frame, text="Join", command=join_chat, bg="#7289DA", fg="white")
